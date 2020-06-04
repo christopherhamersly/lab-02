bdc8b6698aa758bb72b16fbfd277a1a0216b23cb
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2020-06-03 19:33:07 -0700

    sadly section four not finished

--- README.md
+++ README.md
@@ -20,18 +20,10 @@ Estimate of time needed to complete: 8 Hours
 
 Start time: 14.00
 
-Finish time: __
+Finish time: 19.00
 
-Actual time needed to complete: __ 
+Actual time needed to complete: Not completed yet.  Still working out some of the bugs.  5 hours in. Completed three of the four features. 
 
 
-<!-- 1. Add another page -->
-<!-- Create a button for that other page -->
-<!-- fix the keywords -->
 
-2. create the appropriate mustache template in the HTML with the same <h2>, <img>, and <p>
-
-<!-- 3. style using flexboxes -->
-
-4. sort the images by title and number of horns. 
 
--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-/* eslint-disable indent */
-'use strict';
 
+'use strict';
+//global variables
 let allHorns =[];
 let keywordArray = [];
 const newTemplate = $('#photo-template').html();
 
-
+//constructor function
 function Horn(obj){
   this.title = obj.title;
   this.keyword = obj.keyword;
@@ -15,21 +15,28 @@ function Horn(obj){
   allHorns.push(this);
 }
 
-Horn.prototype.render = function (){
-  const $newSection = $(`<section>${newTemplate}</section>`);
-  $newSection.find('h2').text(this.keyword);
-  $newSection.find('p').text(this.description);
-  $newSection.find('img').attr('src', this.image_url);
-  $newSection.find('img').attr('title', this.title);
-  $('main').append($newSection);
-};
+// Horn.prototype.render = function (){
+//   const $newSection = $(`<section>${newTemplate}</section>`);
+//   $newSection.find('h2').text(this.keyword);
+//   $newSection.find('p').text(this.description);
+//   $newSection.find('img').attr('src', this.image_url);
+//   $newSection.find('img').attr('title', this.title);
+//   $('main').append($newSection);
+// };
+
+//render sections using {{Mustache}}
+Horn.prototype.render = function(){
+  const template = $('#mustache-template').html();
+  const myTemplate = Mustache.render(template, this);
+  $('main').append(myTemplate);
+}
 
 
 //synchronus code
 $(document).ready(function(){
 
-//pull images and info from JSON
-$.ajax('data/page-1.json', {method: 'GET', dataType:'JSON'})
+  //pull images and info from JSON
+  $.ajax('data/page-1.json', {method: 'GET', dataType:'JSON'})
     .then(allHornAnimal => {
       allHornAnimal.forEach(value => {
         new Horn(value).render();
@@ -48,14 +55,42 @@ let keywordFiller = (obj) => {
     }
   });
 };
-//filling the selector box
+
+//rendering images to the DOM for page two
+const ajaxPopulater = () => {
+  $.ajax('data/page-2.json', {method: 'GET', dataType:'JSON'})
+    .then(allHornAnimal => {
+      allHornAnimal.forEach(value => {
+        new Horn(value).render();
+        keywordFiller(allHorns);
+      });
+      keywordFiller(allHorns);
+      populateBox();
+    });
+};
+
+//rendering images to the DOM for page one
+const ajaxPopulaterOne = () => {
+  $.ajax('data/page-1.json', {method: 'GET', dataType:'JSON'})
+    .then(allHornAnimal => {
+      allHornAnimal.forEach(value => {
+        new Horn(value).render();
+        keywordFiller(allHorns);
+      });
+      keywordFiller(allHorns);
+      populateBox();
+    });
+  $('main').empty();
+};
+
+//rendering the selection box
 const boxFiller = () => {
   $('select').empty();
   let $newOption = $(`<option></option>`);
   $newOption.text('Filter By Category');
   $newOption.attr('value', 'default');
   $('select').append($newOption);
-//looping over keyword array 
+  //looping over keyword array
   keywordArray.forEach(value => {
     $newOption = $(`<option>${value}</option>`);
     $newOption.attr(`value`, `${value}`);
@@ -63,24 +98,6 @@ const boxFiller = () => {
   });
 }
 
-//sorting the horns
-const hornSort = () => {
-  allHorns.sort(( a, b ) => {
-  return a.horns > b.horns ? 1 : -1;
-  });
-}
-
-//sorting the titles
-const titleSort = () => {
-  allHorns.sort(( a, b ) =>{
-    if (a.toLowerCase() > b.toLowerCase){
-      return 1;
-    } else if (a.toLowerCase() < b.toLowerCase()){
-      return -1;
-    }
-    return allHorns;
-  });
-}
 //filling the selector box
 const populateBox = () => {
   keywordFiller(allHorns);
@@ -100,80 +117,72 @@ $('select').on('change', function(event){
     $('main').empty();
     allHorns.forEach(value => value.render());
   }
+  boxFiller;
 });
 
+//sorting the horns
+const hornSort = () => {
+  allHorns.sort(( a, b ) => {
+    return a.horns > b.horns ? 1 : -1;
+  });
+  return allHorns;
+}
+
+//sorting the titles
+const titleSort = () => {
+  allHorns.sort(( a, b ) =>{
+    if (a.toLowerCase() > b.toLowerCase){
+      return 1;
+    } else if (a.toLowerCase() < b.toLowerCase()){
+      return -1;
+    }
+  });
+  return allHorns;
+}
+
+//event listener for page one
 $('#button1').on('click', function(){
   // wipe the previous shown data
-    $('main').empty();
+  $('main').empty();
   // populate it with information
-    $.ajax('data/page-1.json', {method: 'GET', dataType:'JSON'})
-    .then(allHornAnimal => {
-      allHornAnimal.forEach(value => {
-        new Horn(value).render();
-        keywordFiller(allHorns);
-      });
-      keywordFiller(allHorns);
-      populateBox();
-    });
-  });
-
-  // create an event listener
-  $('#button2').on('click', function(){
-    // wipe the previous shown data
-    $('main').empty();
-    // populate it with information
-    ajaxPopulater();
-  });
-
-  $('hornsorter').on('click', function(){
-    $('main').empty();
-    hornSort;
-    ajaxPopulater;
-  })
+  ajaxPopulaterOne();
+});
 
 
-  const ajaxPopulater = () => {
-    $.ajax('data/page-2.json', {method: 'GET', dataType:'JSON'})
-    .then(allHornAnimal => {
-      allHornAnimal.forEach(value => {
-        new Horn(value).render();
-        keywordFiller(allHorns);
-      });
-      keywordFiller(allHorns);
-      populateBox();
-    });
-  };
+// event listener for page two
+$('#button2').on('click', function(){
+  // wipe the previous shown data
+  $('main').empty();
+  // populate it with information
+  ajaxPopulater();
+});
+
+
+//event listener for horn sort button
+$('#hornsorter').on('click', function(){
+  // wipe the previous shown data
+  event.preventDefault();
+  //fill it with data
+  // hornSort(allHorns);
+ 
+  // populate it with information
+  ajaxPopulaterOne(hornSort);
+});
+
+$('#titlesorter').on('click', function(){
+  // wipe the previous shown data
+  event.preventDefault();
+  //fill it with data
+  // hornSort(allHorns);
+  $('main').empty();
+  // populate it with information
+  ajaxPopulater(titleSort);
+});
 
 
 
 // create sorting for the horns array
 
 
-
-
 //publish the content to the page 
- 
-$('#hornsorter').on('click', function(){
-  // wipe the previous shown data
-  event.preventDefault();
-  hornSort();
 
-
-
-
-
-
-
-  $('main').empty();
-  // populate it with information
-  $.ajax('data/page-2.json', {method: 'GET', dataType:'JSON'})
-  .then(allHornAnimal => {
-    allHornAnimal.forEach(value => {
-      new Horn(value).render();
-      keywordFiller(allHorns);
-    });
-    keywordFiller(allHorns);
-    populateBox();
-  });
-
-});
